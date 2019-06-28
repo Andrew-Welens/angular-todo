808a07c985bebfe4c3e6f04e96d98e5f9e8e957d
Welens <48022793+Andrew-Welens@users.noreply.github.com>
2019-06-28 19:01:41 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  appTitle: string = 'First ng app';
+  developer: string = 'Andrew Welens';
+}
